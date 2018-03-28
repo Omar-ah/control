fbe5824ec4c71295d7cb9ddd5de2eb2787f9fffc
Omar <brightside996@gmail.com>
2018-03-28 22:14:00 +0200

    initial commit with just the basic layout

---
+++ app/src/main/java/goods/mine/com/control/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package goods.mine.com.control.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import goods.mine.com.control.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
